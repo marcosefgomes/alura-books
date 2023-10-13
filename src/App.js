b3f987b6464761de178dbd604321cd5270acad1c
marcosefgomes <m4rc05.g0m35@gmail.com>
2023-10-13 02:03:53 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Header from "./componentes/Header";
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
+
+  li {
+    list-style: none;
+  }
+`;
+
+function App() {
+  return (
+    <AppContainer>
+      <Header />
+    </AppContainer>
+  );
+}
+
+export default App;
